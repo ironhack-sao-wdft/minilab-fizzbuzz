25b0c37f1e1ea80527a6ba3b3ce4472b0fb59621
Larissa Ferreira <larissadantasaf@gmail.com>
2021-08-10 19:57:30 +0200

    larissa-exercise

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Seu código aqui!
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 == 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
