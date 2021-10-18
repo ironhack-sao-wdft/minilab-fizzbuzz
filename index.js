59ea0fa3ca4f79e04cfe92a7c0db94b493f9bf82
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-10-18 19:14:47 -0300

    innitial commit - wrote the js code

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-// Seu código aqui!
-
-let count = 1;
-
-while (count <= 100) {
-  if (i % 3 === 0) {
-    console.log("fizz");
-  }
-
-  count++;
+for (let counter = 1; counter <= 100; counter++) {
+  const multiploTres = (counter % 3 === 0)
+  const multiploCinco = (counter % 5 === 0)
+  const multiploTresECinco = (counter % 3 === 0) && (counter % 5 === 0)
+    if (multiploTresECinco) {
+      console.log('fizz buzz')
+    } else if (multiploTres) {
+      console.log('fizz')
+    } else if (multiploCinco) {
+      console.log('buzz')
+    } else {
+      console.log(counter)
+    }
 }
