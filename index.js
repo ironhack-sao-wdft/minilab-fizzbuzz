62f10406d28b7fa069afcb5a3583f41e50563ab6
Guilherme Santos Gomes <guilherme0500@hotmail.com>
2021-10-19 00:26:38 -0300

    MINILAB | Fizz Buzz

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let count = 1;
 
 while (count <= 100) {
-  if (count % 3 === 0) {    
+    
+  if (count % 3 === 0 && count % 5 === 0) {    
+    console.log("fizz buzz");    
+  } else if (count % 3 === 0) {    
     console.log("fizz");    
   } else if (count % 5 === 0){
     console.log("buzz");
